362eb8d12caaf1aa1d3fd4a6297c3e5b6db96d21
chris <drikosx323@gmail.com>
2024-03-29 17:14:02 +0200

    Implemented TreeNode class for Binary Search Tree

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    private TreeNode root;
+
+    private class TreeNode{
+        private  int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
