4dceba386c203a01bc4d32fbb63f662f79510cc4
chris <drikosx323@gmail.com>
2024-03-30 14:31:32 +0200

    Implemented insert method and inOrder print

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,43 @@ public class Main {
         }
     }
 
+    public void insert(int value){
+        root = insert(root,value);
+    }
+    public TreeNode insert(TreeNode root, int value){
+        if (root == null){
+            root = new TreeNode(value);
+            return root;
+        }
+        if (value < root.data){
+            root.left = insert(root.left, value);
+        }else{
+            root.right = insert(root.right,value);
+        }
+        return root;
+    }
+
+//    public void inOrder(){
+//        inOrder(root);
+//    }
+    public void inOrder(TreeNode root){
+        if (root == null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Main sll = new Main();
+        sll.insert(5);
+        sll.insert(3);
+        sll.insert(7);
+        sll.insert(1);
+
+        sll.inOrder(sll.root);
+
     }
 }
